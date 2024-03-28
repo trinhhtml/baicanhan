ccb5f9e2ceefa8fe71210cdc9d6d1e2e0f3aaba7
trinhhtml <164768957+trinhhtml@users.noreply.github.com>
2024-03-28 23:06:58 +0700

    Add files via upload

---
+++ controls.js
@@ -0,0 +1,7 @@
+// JavaScript Document$('.dot1').click(function(){		
+
+$('.dot1').click(function(){
+	$('video1').css('display','block');
+		$('video2').css('display','none');
+		$('video3').css('display','none');
+})
